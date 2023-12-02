79b5cab13ea91e31ca774844660b0395946a4bf6
williamdeli <101968925+williamdeli@users.noreply.github.com>
2023-12-03 04:09:45 +0700

    Update streamlit_project_heart_disease.py

--- streamlit_project_heart_disease.py
+++ streamlit_project_heart_disease.py
@@ -4,9 +4,6 @@ import pickle
 import time
 import numpy as np
 from PIL import Image
-import subprocess
-scikit_learn_version = "1.2.2"
-subprocess.check_call(["pip", "install", f"scikit-learn=={scikit_learn_version}"])
 
 #page configuration
 st.set_page_config(
